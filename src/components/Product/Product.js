25894c56e98c0ac302c4e7951d163c548d366e52
khanhnhb <khanhnhbse@gmail.com>
2021-12-03 00:36:21 +0700

    Responsive Home Product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import { currencyFormat } from '../../utils/CurrencyFormat';
 import "./Product.css";
 
 function Product({ id, name, image, desc, price, sold_number }) {
 
     return (
         <div className="product">
-            <div className="product__info">
-                <img
-                    className="product__image"
-                    src={image}
-                    alt="" />
-                <p className="product__name">{name}</p>
-                <p className="product__desc">{desc}</p>
-                <p className="product__price">
-                    <small>{price}</small>
-                    <small className="product__currency_unit">VNĐ</small>
-                </p>
-                <div className="product__sold">
-                    <p>Đã Bán:</p>
-                    <p className="product__sold__evident">{sold_number}</p>
-                </div>
+            <img
+                className="product__image"
+                src={image}
+                alt="" />
+            <p className="product__name">{name}</p>
+            <p className="product__desc">{desc}</p>
+            <p className="product__price">
+                <small>{currencyFormat(price)}</small>
+                <small className="product__currency_unit">đ</small>
+            </p>
+            <div className="product__sold">
+                <p>Đã Bán:</p>
+                <p className="product__sold__evident">{sold_number}</p>
             </div>
         </div>
     )
